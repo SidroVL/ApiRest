bd0c30b56c6d5279f57d601bdd87284e0dae9af8
unknown <DAM229@ACARBALLEIRA.LOCAL>
2018-11-16 14:21:36 +0100

    usuarios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Proveedor1Provider } from './../../providers/proveedor1/proveedor1'; //automatico si todo va bien
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  usuarios //variable para almacenar los datos
+  constructor(public navCtrl: NavController, public proveedor:Proveedor1Provider) {
 
+  }
+
+  ionViewDidLoad(){
+   this.proveedor.obtenerDatos()
+   .subscribe(
+     (data)=>{this.usuarios=data;},
+     (error)=>{console.log(error);}
+   )
   }
 
 }
